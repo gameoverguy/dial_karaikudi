8fcff2f3f2bcaaecac7c6a2e83559777e1451b9d
gameoverguy <karthick251087@gmail.com>
2024-12-14 19:08:47 +0530

    office modifications

--- Dial Karaikudi/index.js
+++ Dial Karaikudi/index.js
@@ -1,22 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const navlink = document.querySelectorAll('.navbar a');
-    const top_panel = document.getElementById("top_panel");
+    const menu = document.querySelector('.navbar');
+    let isMenuShowing = false;
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
-            top_panel.classList.add('scrolled_top');
-            navlink.forEach(element => {
-                element.classList.add("scrolled")
-            });
+    function Menu_Button(){
+        if(isMenuShowing == false){
+            menu.classList.add("showMenu");
+            isMenuShowing = true;
         }
-        else {
-            top_panel.classList.remove('scrolled_top');
-            navlink.forEach(element => {
-                element.classList.remove("scrolled")
-            });
+        else{
+            menu.classList.remove("showMenu");
+            isMenuShowing = false;
         }
-    });
-
+    }
 
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const navlink = document.querySelectorAll('.navbar a');
+//     const top_panel = document.getElementById("top_panel");
+
+//     window.addEventListener('scroll', () => {
+//         if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+//             top_panel.classList.add('scrolled_top');
+//             navlink.forEach(element => {
+//                 element.classList.add("scrolled")
+//             });
+//         }
+//         else {
+//             top_panel.classList.remove('scrolled_top');
+//             navlink.forEach(element => {
+//                 element.classList.remove("scrolled")
+//             });
+//         }
+//     });
+
+
+// });
+
+
