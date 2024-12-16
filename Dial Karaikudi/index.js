fcf64c3c762662f9c6921e42ea6032579d80e219
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2024-12-16 08:58:02 +0530

    home edit

--- Dial Karaikudi/index.js
+++ Dial Karaikudi/index.js
@@ -1,43 +1,43 @@
+const menu = document.querySelector('.navbar');
+let isMenuShowing = false;
+
+function Menu_Button() {
+
+    console.log("test");
+
+    if (isMenuShowing == false) {
+        menu.classList.add("showMenu");
+        isMenuShowing = true;
+    }
+    else {
+        menu.classList.remove("showMenu");
+        isMenuShowing = false;
+    }
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const menu = document.querySelector('.navbar');
-    let isMenuShowing = false;
+    const navlink = document.querySelectorAll('.navbar a');
+    const top_panel = document.getElementById("top_panel");
 
-    function Menu_Button(){
-        if(isMenuShowing == false){
-            menu.classList.add("showMenu");
-            isMenuShowing = true;
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+            top_panel.classList.add('scrolled_top');
+            navlink.forEach(element => {
+                element.classList.add("scrolled")
+            });
         }
-        else{
-            menu.classList.remove("showMenu");
-            isMenuShowing = false;
+        else {
+            top_panel.classList.remove('scrolled_top');
+            navlink.forEach(element => {
+                element.classList.remove("scrolled")
+            });
         }
-    }
+    });
+
 
 });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-//     const navlink = document.querySelectorAll('.navbar a');
-//     const top_panel = document.getElementById("top_panel");
-
-//     window.addEventListener('scroll', () => {
-//         if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
-//             top_panel.classList.add('scrolled_top');
-//             navlink.forEach(element => {
-//                 element.classList.add("scrolled")
-//             });
-//         }
-//         else {
-//             top_panel.classList.remove('scrolled_top');
-//             navlink.forEach(element => {
-//                 element.classList.remove("scrolled")
-//             });
-//         }
-//     });
-
-
-// });
 
 
