b98bc362cc580a7a3202c898ae49fb2644e3412d
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-01-10 21:41:05 +0530

    edit

--- Dial Karaikudi/index.js
+++ Dial Karaikudi/index.js
@@ -1,46 +1,49 @@
-const menu = document.querySelector('.navbar');
+const menu = document.querySelector(".navbar");
 const menubutton = document.querySelector(".menu");
+const menuoverlay = document.querySelector(".menu_overlay");
 let isMenuShowing = false;
 
 function Menu_Button() {
+  const menulogo = menubutton.querySelector("img");
 
-    const menulogo = menubutton.querySelector('img');
-
-    if (isMenuShowing == false) {
-        menu.classList.add("showMenu");
-        menulogo.classList.add("menulogo");
-        isMenuShowing = true;
-    }
-    else {
-        menu.classList.remove("showMenu");
-        menulogo.classList.remove("menulogo");
-        isMenuShowing = false;
-    }
+  if (isMenuShowing == false) {
+    menu.classList.add("showMenu");
+    menulogo.classList.add("menulogo");
+    menuoverlay.classList.add("show");
+    isMenuShowing = true;
+  } else {
+    menu.classList.remove("showMenu");
+    menulogo.classList.remove("menulogo");
+    menuoverlay.classList.remove("show");
+    isMenuShowing = false;
+  }
 }
 
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  console.log(section);
 
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener("DOMContentLoaded", () => {
+//   const navlink = document.querySelectorAll(".navbar a");
+//   const top_panel = document.getElementById("top_panel");
 
-    const navlink = document.querySelectorAll('.navbar a');
-    const top_panel = document.getElementById("top_panel");
-
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
-            top_panel.classList.add('scrolled_top');
-            navlink.forEach(element => {
-                element.classList.add("scrolled")
-            });
-        }
-        else {
-            top_panel.classList.remove('scrolled_top');
-            navlink.forEach(element => {
-                element.classList.remove("scrolled")
-            });
-        }
-    });
-
-
-});
-
-
+//   window.addEventListener("scroll", () => {
+//     if (window.scrollY > 50) {
+//       // Adjust this value based on when you want the color to change
+//       // top_panel.classList.add('scrolled_top');
+//       navlink.forEach((element) => {
+//         element.classList.add("scrolled");
+//       });
+//     } else {
+//       // top_panel.classList.remove('scrolled_top');
+//       navlink.forEach((element) => {
+//         element.classList.remove("scrolled");
+//       });
+//     }
+//   });
+// });
